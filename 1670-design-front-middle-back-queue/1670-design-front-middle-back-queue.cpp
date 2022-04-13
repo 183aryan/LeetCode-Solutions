d47808896b4345e2a00f980f34825a546f510de3
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-04-13 23:54:41 +0530

    Time: 91 ms (5.22%), Space: 48.4 MB (5.22%) - LeetHub

--- 1670-design-front-middle-back-queue/1670-design-front-middle-back-queue.cpp
+++ 1670-design-front-middle-back-queue/1670-design-front-middle-back-queue.cpp
@@ -1,53 +1,85 @@
-class FrontMiddleBackQueue {
+class FrontMiddleBackQueue
+{
     deque<int> q;
-public:
-    FrontMiddleBackQueue() {
-        deque<int> q;
-    }
-    
-    void pushFront(int val) {
+    public:
+        FrontMiddleBackQueue()
+        {
+            deque<int> q;
+        }
+
+    void pushFront(int val)
+    {
         q.push_front(val);
     }
-    
-    void pushMiddle(int val) {
+
+    void pushMiddle(int val)
+    {
+       	// int len = q.size();
+       	// deque<int>::iterator it = q.begin();
+       	// len = len/2;
+       	// q.insert(it+len,val);
+
         int len = q.size();
-        deque<int>::iterator it = q.begin();
-        len = len/2;
-        q.insert(it+len,val);
+        len = len / 2;
+
+        stack<int> s;
+        for (int i = 0; i < len; i++)
+        {
+            s.push(q.front());
+            q.pop_front();
+        }
+        q.push_front(val);
+        while (!s.empty())
+        {
+            q.push_front(s.top());
+            s.pop();
+        }
+        return;
     }
-    
-    void pushBack(int val) {
+
+    void pushBack(int val)
+    {
         q.push_back(val);
     }
-    
-    int popFront() {
-        if(q.size() == 0) return -1;
+
+    int popFront()
+    {
+        if (q.size() == 0) return -1;
         int val = q.front();
         q.pop_front();
         return val;
     }
-    
-    int popMiddle() {
-        if(q.size() == 0) return -1;
+
+    int popMiddle()
+    {
+        if (q.size() == 0) return -1;
         int len = q.size();
-        if(len%2) len = len/2;
-        else{ len = len/2; len--;}
-        
+        if (len % 2) len = len / 2;
+        else
+        {
+            len = len / 2;
+            len--;
+        }
+
         stack<int> s;
-        for(int i=0;i<=len;i++){
-            s.push(q.front());q.pop_front();
+        for (int i = 0; i <= len; i++)
+        {
+            s.push(q.front());
+            q.pop_front();
         }
         int val = s.top();
         s.pop();
-        while(!s.empty()){
+        while (!s.empty())
+        {
             q.push_front(s.top());
             s.pop();
         }
         return val;
     }
-    
-    int popBack() {
-        if(q.size() == 0) return -1;
+
+    int popBack()
+    {
+        if (q.size() == 0) return -1;
         int val = q.back();
         q.pop_back();
         return val;
@@ -55,12 +87,12 @@ public:
 };
 
 /**
- * Your FrontMiddleBackQueue object will be instantiated and called as such:
- * FrontMiddleBackQueue* obj = new FrontMiddleBackQueue();
- * obj->pushFront(val);
- * obj->pushMiddle(val);
- * obj->pushBack(val);
- * int param_4 = obj->popFront();
- * int param_5 = obj->popMiddle();
- * int param_6 = obj->popBack();
+ *Your FrontMiddleBackQueue object will be instantiated and called as such:
+ *FrontMiddleBackQueue* obj = new FrontMiddleBackQueue();
+ *obj->pushFront(val);
+ *obj->pushMiddle(val);
+ *obj->pushBack(val);
+ *int param_4 = obj->popFront();
+ *int param_5 = obj->popMiddle();
+ *int param_6 = obj->popBack();
  */
