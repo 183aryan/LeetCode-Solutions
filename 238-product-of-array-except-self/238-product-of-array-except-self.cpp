5863a544e209297891bd1871966b47b37fa06efd
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-04-14 18:04:07 +0530

    Time: 29 ms (66.78%), Space: 25.6 MB (9.31%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -5,9 +5,13 @@ class Solution
         {
             vector<int> v;
             int n = nums.size();
+            // pre computation....
+            
+            // declared left and right array for storing left and right product respectively.
             int left[n], right[n];
             int prodL = 1, prodR = 1;
 
+            // storing left product in left array.
             left[0] = 1;
             for (int i = 1; i < n; i++)
             {
@@ -15,6 +19,7 @@ class Solution
                 left[i] = prodL;
             }
 
+            // storing right product in right array.
             right[n - 1] = 1;
             for (int i = n - 2; i >= 0; i--)
             {
@@ -22,6 +27,7 @@ class Solution
                 right[i] = prodR;
             }
 
+            // main computation for the result.....
             for (int i = 0; i < n; i++)
             {
                 v.push_back(left[i] *right[i]);
