1ed8f3ac8dbfefa4763b3a22cb07816d613fb9b5
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-04-14 18:01:52 +0530

    Time: 24 ms (85.15%), Space: 25.5 MB (9.31%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,40 +1,32 @@
-class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-//         vector<int> v;
-//         int product = 1;
-//         for(int i=0;i<nums.size();i++){
-//             product *= nums[i];
-//         }
-        
-//         for(int i=0;i<nums.size();i++){
-//             if(nums[i] == 0){
-//                 v.push_back()
-//             }
-//             else v.push_back(product/nums[i]);
-//         }
-//         return v;
-        vector<int> v;
-        int n = nums.size();
-        int left[n],right[n];
-        int prodL = 1, prodR = 1;
-        
-        left[0] = 1;
-        for(int i=1;i<n;i++){
-            prodL = prodL * nums[i-1];
-            left[i] = prodL;
+class Solution
+{
+    public:
+        vector<int> productExceptSelf(vector<int> &nums)
+        {
+            vector<int> v;
+            int n = nums.size();
+            int left[n], right[n];
+            int prodL = 1, prodR = 1;
+
+            left[0] = 1;
+            for (int i = 1; i < n; i++)
+            {
+                prodL = prodL *nums[i - 1];
+                left[i] = prodL;
+            }
+
+            right[n - 1] = 1;
+            for (int i = n - 2; i >= 0; i--)
+            {
+                prodR = prodR *nums[i + 1];
+                right[i] = prodR;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                v.push_back(left[i] *right[i]);
+            }
+
+            return v;
         }
-        
-        right[n-1] = 1;
-        for(int i=n-2;i>=0;i--){
-            prodR = prodR*nums[i+1];
-            right[i] = prodR;
-        }
-        
-        for(int i=0;i<n;i++){
-            v.push_back(left[i]*right[i]);
-        }
-        
-        return v;
-    }
 };
