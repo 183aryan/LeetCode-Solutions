2494185550f123505ea90e9671a77eda5ed1920c
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-04-19 18:55:43 +0530

    Time: 17 ms (38.76%), Space: 21.4 MB (40.71%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -1,22 +1,24 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        // int sum = 0;
-        if(root == NULL) return false; 
-        targetSum -= root->val;
-        // sum += root->val;
-        if(targetSum == 0 and root->left == NULL and root->right == NULL) return true;
-        return (hasPathSum(root->left,targetSum) || hasPathSum(root->right,targetSum));
-    }
+class Solution
+{
+    public:
+        bool hasPathSum(TreeNode *root, int targetSum)
+        {
+           	// int sum = 0;
+            if (root == NULL) return false;
+            targetSum -= root->val;
+           	// sum += root->val;
+            if (targetSum == 0 and root->left == NULL and root->right == NULL) return true;
+            return (hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum));
+        }
 };
