829b193f27a1e30de09ade58f6ac670a3a430c5a
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-04-19 17:58:14 +0530

    Time: 51 ms (40.60%), Space: 57.9 MB (70.41%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -1,35 +1,41 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    TreeNode* prev = NULL;
-    TreeNode* first = NULL;
-    TreeNode* second = NULL;
-    
-    void inorder(TreeNode* root){
-        if(root == NULL) return;
+class Solution
+{
+    public:
+        TreeNode *prev = NULL;
+    TreeNode *first = NULL;
+    TreeNode *second = NULL;
+
+    void inorder(TreeNode *root)
+    {
+        if (root == NULL) return;
         inorder(root->left);
-        if(prev != NULL and prev->val > root->val){  // checking whether inorder is correct or not.....
-            if(first == NULL){
-                first = prev; // mark first juction.....
+        if (prev != NULL and prev->val > root->val)
+        {
+        	// checking whether inorder is correct or not.....
+            if (first == NULL)
+            {
+                first = prev;	// mark first juction.....
             }
-            second = root; // second point.....
+            second = root;	// second point.....
         }
         prev = root;
         inorder(root->right);
     }
-    
-    void recoverTree(TreeNode* root) {
+
+    void recoverTree(TreeNode *root)
+    {
         inorder(root);
-        swap(first->val,second->val);
+        swap(first->val, second->val);
     }
 };
