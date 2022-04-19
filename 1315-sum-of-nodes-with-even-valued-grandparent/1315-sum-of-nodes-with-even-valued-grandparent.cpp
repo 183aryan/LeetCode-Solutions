ec7ce9f2284e8aec30cefe4b3ac5a3706583e1f2
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-04-20 00:48:48 +0530

    Time: 56 ms (68.54%), Space: 42.1 MB (14.44%) - LeetHub

---
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    
+public:
+    
+    int sumEvenGrandparent(TreeNode* root) {
+        
+        queue<TreeNode*> q;
+        q.push(root) ;
+        int ans = 0 ;
+        
+        while(!q.empty())
+        {
+                TreeNode* temp = q.front() ;
+                q.pop() ;
+                
+                if(temp->val%2==0)
+                {
+                    //ll
+                    if(temp->left!=NULL&&temp->left->left!=NULL)
+                        ans+=temp->left->left->val ;
+                    
+                    //lr
+                    if(temp->left!=NULL&&temp->left->right!=NULL)
+                        ans+=temp->left->right->val ;
+                    
+                    //rl
+                    if(temp->right!=NULL&&temp->right->left!=NULL)
+                        ans+=temp->right->left->val ;
+                    
+                    //rr
+                    if(temp->right!=NULL&&temp->right->right!=NULL)
+                        ans+=temp->right->right->val ;
+                    
+                
+                    }
+                    if(temp->left)
+                        q.push(temp->left);
+                    
+                    if(temp->right)
+                        q.push(temp->right);
+            
+        }
+        
+        return ans ;
+    }
+    
+};
