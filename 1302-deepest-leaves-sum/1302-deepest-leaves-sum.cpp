5f910b4f3a776839c2f2075bfed77f5fe5af569d
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-04-19 18:31:08 +0530

    Time: 92 ms (93.41%), Space: 61.9 MB (28.23%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -1,32 +1,36 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    int deepestLeavesSum(TreeNode* root) {
-        if(root == NULL) return 0;
-        queue<TreeNode *> q;
-        q.push(root);
-        int sum;
-        while(q.empty() == false){
-            int cnt = q.size();
-            sum = 0;
-            for(int i=0;i<cnt;i++){
-                TreeNode* x = q.front();
-                q.pop();
-                sum += x->val;
-                if(x->left != NULL) q.push(x->left);
-                if(x->right != NULL) q.push(x->right);
+class Solution
+{
+    public:
+        int deepestLeavesSum(TreeNode *root)
+        {
+            if (root == NULL) return 0;
+            queue<TreeNode*> q;
+            q.push(root);
+            int sum;
+            while (q.empty() == false)
+            {
+                int cnt = q.size();
+                sum = 0;
+                for (int i = 0; i < cnt; i++)
+                {
+                    TreeNode *x = q.front();
+                    q.pop();
+                    sum += x->val;
+                    if (x->left != NULL) q.push(x->left);
+                    if (x->right != NULL) q.push(x->right);
+                }
             }
+            return sum;
         }
-        return sum;
-    }
 };
