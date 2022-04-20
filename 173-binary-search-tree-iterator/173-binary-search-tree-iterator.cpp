f29dfb1dc68885500f7cd332ad6f9e958c481257
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-04-20 17:11:25 +0530

    Time: 26 ms (89.76%), Space: 24.2 MB (56.50%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -11,33 +11,41 @@
  */
 class BSTIterator {
 public:
-    stack<TreeNode *> s1,s2;
+    // stack<TreeNode *> s1,s2;  // when you will make all stack based functions uncommented and vector based commented then will able to get answer(accepted) with use 2 stack method.
+    vector<int> v;    // vector method
+    int i=0;
     BSTIterator(TreeNode* root) {
         inorder(root);
     }
     void inorder(TreeNode* root){
         if(root == NULL) return;
-        inorder(root->left);
-        s1.push(root);
+        inorder(root->left);                   
+        // s1.push(root);                  // stack
+        v.push_back(root->val);            // vector
         inorder(root->right);
     }
-    void check(stack<TreeNode *>& s1,stack<TreeNode *>& s2){
-        while(s1.empty() == false){
-            s2.push(s1.top());
-            s1.pop();
-        }
-    }
+    // void check(stack<TreeNode *>& s1,stack<TreeNode *>& s2){
+    //     while(s1.empty() == false){
+    //         s2.push(s1.top());
+    //         s1.pop();
+    //     }                               // check function is used for 2 stack method.
+    // }
     int next() {
-       check(s1,s2);
-        auto x = s2.top();
-        s2.pop();
-        return x->val;
+       // check(s1,s2);
+        // auto x = s2.top();
+        // s2.pop();                       // 2 stack method
+        // return x->val;
+        int x = v[i];
+        i++;                               // vector method
+        return x;
     }
     
     bool hasNext() {
-        check(s1,s2);
-        if(s2.empty()) return false;
-        return true;
+        // check(s1,s2);
+        // if(s2.empty()) return false;   // 2 stack method
+        // return true;
+        if(i<v.size()) return true;
+        return false;                     // vector method
     }
 };
 
