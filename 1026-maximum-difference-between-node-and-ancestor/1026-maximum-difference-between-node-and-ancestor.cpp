aa80570924e44bd93a0cb4c43ff45190f2af172d
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-04-23 17:28:41 +0530

    Time: 4 ms (83.19%), Space: 10.9 MB (27.15%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // in this question we are finding minimum and maximum from every subtree and keep on updating ans with max difference value.
+    int ans = INT_MIN;
+    void find(TreeNode* root, int mn, int mx){
+        if(root == NULL) return;
+        ans = max({ans,root->val-mn,mx-root->val});
+        mn = min(mn,root->val);
+        mx = max(mx,root->val);
+        find(root->left,mn,mx);
+        find(root->right,mn,mx);
+    }
+    int maxAncestorDiff(TreeNode* root) {
+        if(root == NULL) return 0;
+        find(root,root->val,root->val);
+        return ans;
+    }
+};
