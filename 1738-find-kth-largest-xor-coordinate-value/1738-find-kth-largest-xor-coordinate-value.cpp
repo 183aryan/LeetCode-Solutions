b7f79b8af8f9495c810238c0bf9f3bb090964113
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-05-15 00:00:04 +0530

    Time: 657 ms (66.57%), Space: 99.2 MB (68.30%) - LeetHub

--- 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.cpp
+++ 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int kthLargestValue(vector<vector<int>>& matrix, int k) {
-        // for finding all xor of the matrix we have to do xor of the matrix in row and column wise fashion.
+        // for finding all xor of the matrix...
         int n = matrix.size();
         int m = matrix[0].size();
         
-        // row
+        // firstly in the matrix.
         for(int i=0;i<n;i++){
             for(int j=1;j<m;j++){
                 matrix[i][j] ^= matrix[i][j-1];
             }
         }
         
-        // column
+        // now 3X5 -> 5X3 me.....for overall xor.....of a element.
         for(int i=0;i<m;i++){
             for(int j=1;j<n;j++){
                 matrix[j][i] ^= matrix[j-1][i];
