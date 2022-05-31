18c9d1fd2e828b969cb0f20c75060aaec4cfb7f6
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-05-31 16:44:36 +0530

    Time: 440 ms (55.34%), Space: 71.6 MB (51.89%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -4,7 +4,7 @@ public:
         
         if(k > s.size()) return false;
         int total = 1 << k;
-        set<string> st;
+        unordered_set<string> st;
         for(int i=0;i<=s.size()-k;i++){
             st.insert(s.substr(i,k));
         }
