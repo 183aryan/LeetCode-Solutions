e2fad7397484aadd11595cc78f661c1da8f4a4ca
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-06-05 12:24:20 +0530

    Added solution - LeetHub

---
+++ Bike Racing - GFG/bike-racing.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    long buzzTime(long N, long M, long L, long H[], long A[])
+    {
+        // code here
+        long lo = 0, hi = M,mid,ans=0;
+        while(hi >= lo){
+            mid = lo + (hi-lo)/2;
+            long fast = 0;
+            for(int i=0;i<N;i++){
+                if(H[i]+A[i]*mid >= L) fast += H[i]+A[i]*mid;
+            }
+            if(fast >= M){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long N, M, L;
+        cin>>N>>M>>L;
+        long H[N], A[N];
+        for(long i = 0;i < N;i++) 
+            cin>>H[i]>>A[i];
+        
+        Solution ob;
+        cout<<ob.buzzTime(N, M, L, H, A)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
