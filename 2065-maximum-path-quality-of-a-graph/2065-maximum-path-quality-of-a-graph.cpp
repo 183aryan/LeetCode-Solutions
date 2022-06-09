9c0ae82155d2db1f72862dc0756e12c15cfd44a2
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-06-09 18:09:47 +0530

    Time: 234 ms (69.42%), Space: 21.6 MB (41.59%) - LeetHub

--- 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.cpp
+++ 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void dfs(vector<vector<pair<int,int>>>& graph,vector<int>& visited,int& res,vector<int>& values,int node,int score,int time,int& maxTime){
+    void dfs(vector<pair<int,int>> adj[],vector<int>& visited,int& res,vector<int>& values,int node,int score,int time,int& maxTime){
         if(time > maxTime) return;
         if(visited[node] == 0) score += values[node];
         
@@ -8,10 +8,10 @@ public:
         
         if(node == 0) res = max(res,score);
         
-        for(auto it:graph[node]){
+        for(auto it:adj[node]){
             int x = it.first;
             int t = it.second;
-            dfs(graph,visited,res,values,x,score,time+t,maxTime);
+            dfs(adj,visited,res,values,x,score,time+t,maxTime);
         }
         visited[node]--;
     }
@@ -19,14 +19,14 @@ public:
         int n = values.size();
         int res = values[0];
         
-        vector<vector<pair<int,int>>> graph(n);
-        for(int i = 0;i<edges.size();i++){
-            graph[edges[i][0]].push_back({edges[i][1],edges[i][2]});
-            graph[edges[i][1]].push_back({edges[i][0],edges[i][2]});
+        vector<pair<int,int>> adj[n];
+        for(auto e:edges){
+            adj[e[0]].push_back({e[1],e[2]});
+            adj[e[1]].push_back({e[0],e[2]});
         }
         
         vector<int> visited(n,0);
-        dfs(graph,visited,res,values,0,0,0,maxTime);
+        dfs(adj,visited,res,values,0,0,0,maxTime);
         return res;
     }
 };
