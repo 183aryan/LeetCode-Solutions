eaf93f8d68ad3d4a1301c8eca10dc6ad4ac1dabc
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-06-11 19:07:53 +0530

    Time: 4 ms (89.06%), Space: 8.7 MB (54.82%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> front(n+1,0),curr(n+1,0);
+        for(int j=0;j<n;j++) front[j] = triangle[n-1][j];
+    
+        for(int i=n-2;i>=0;i--){
+           for(int j=i;j>=0;j--){
+              int down = triangle[i][j] + front[j];
+              int diagonal = triangle[i][j] + front[j+1];
+              curr[j] = min(down,diagonal);
+           }
+           front = curr; 
+        }
+    
+        return front[0];
+    }
+};
