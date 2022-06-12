801c1561bd11a8eb30cfb900a091e2748e31d59a
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-06-12 11:22:54 +0530

    Time: 806 ms (10.58%), Space: 156.5 MB (8.75%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        set<int> s;
+        int l=0,r=0,sum = 0,res=0;
+        while(r < nums.size()){
+            sum += nums[r];
+            while(s.find(nums[r]) != s.end()){
+                sum -= nums[l];
+                s.erase(nums[l]);
+                l++;
+            }
+            s.insert(nums[r]);
+            r++;
+            res = max(res,sum);
+        }
+        return res;
+    }
+};
