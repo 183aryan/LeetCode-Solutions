1da960d5b1c72b76c7bb36dd3db346968c2f9505
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-07-04 23:58:22 +0530

    Time: 24 ms (84.37%), Space: 17.8 MB (40.68%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        
+        if(n <= 1) return n;
+        vector<int> v(n,1);
+        
+        for(int i=1;i<n;i++){
+            if(ratings[i] > ratings[i-1]) v[i] = v[i-1] + 1;
+        }
+        
+        for(int i=n-1;i>0;i--){
+            if(ratings[i-1] > ratings[i]) v[i-1] = max(v[i]+1,v[i-1]);
+        }
+        
+        int res = 0;
+        for(int i=0;i<n;i++){
+            res += v[i];
+        }
+        
+        return res;
+    }
+};
