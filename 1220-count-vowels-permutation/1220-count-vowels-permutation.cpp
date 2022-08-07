ff8042dc3188c3963aa7a28ad275cd941ca83285
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-08-07 23:48:14 +0530

    Time: 401 ms (11.68%), Space: 20 MB (46.57%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    unordered_map<char,vector<char>> m ={ {'s',{'a','e','i','o','u'}},{'a',{'e'}},{'e',{'a','i'}},{'i',{'a','e','o','u'}},{'o',{'i','u'}},{'u',{'a'}}};
+    
+    unordered_map<char,vector<int>> dp;
+    
+    int f(int n,char used){
+        if(n == 0) return 1;
+        
+        if(dp[used][n]) return dp[used][n];
+        
+        for(auto x:m.at(used)){
+            dp[used][n] = (dp[used][n] + f(n-1,x))%mod;
+        }
+        
+        return dp[used][n];
+    }
+    int countVowelPermutation(int n) { 
+        dp['s'] = vector<int>(n+1);
+        dp['a'] = vector<int>(n+1);
+        dp['e'] = vector<int>(n+1);
+        dp['i'] = vector<int>(n+1);
+        dp['o'] = vector<int>(n+1);
+        dp['u'] = vector<int>(n+1);
+        
+        return f(n,'s');
+    }
+};
