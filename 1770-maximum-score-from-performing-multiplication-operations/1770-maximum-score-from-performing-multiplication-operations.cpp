8b28fba350578853a39ef62d9b55390304343239
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-09-16 22:06:04 +0530

    Time: 989 ms (17.36%), Space: 336.3 MB (5.74%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int f(int i, int l, vector<int>& nums, vector<int>& multipliers, vector<vector<int>>& dp){
+        int n = nums.size();
+        if(i == multipliers.size()) return 0;
+        if(dp[l][i] != INT_MIN) return dp[l][i];
+        
+        int r = (n-1)-(i-l);
+        
+        int first = nums[l]*multipliers[i] + f(i+1,l+1,nums,multipliers,dp);
+        int last = nums[r]*multipliers[i] + f(i+1,l,nums,multipliers,dp);
+        
+        return dp[l][i] = max(first, last);
+    }
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+         int m=multipliers.size();
+        int n=nums.size();
+        vector<vector<int>> dp(1007,vector<int> (1007,INT_MIN));
+        return f(0,0,nums,multipliers,dp);
+    }
+};
