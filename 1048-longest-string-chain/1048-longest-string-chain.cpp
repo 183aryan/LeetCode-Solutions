4abce34039c341e27e2efc35c2ba549a9eb0a0da
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-09-27 00:14:54 +0530

    Time: 221 ms (35.24%), Space: 13.2 MB (88.24%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-    bool static compare(string &s1,string &s2)
-    {
-         return s1.size() > s2.size();
+    bool check(string& s1, string&s2){
+        if(s1.size() != s2.size()+1) return false;
+        
+        int first=0,second=0;
+        while(first<s1.size()){
+            if(s1[first] == s2[second] and second < s2.size()){
+                first++;
+                second++;
+            }
+            else first++;
+        }
+        
+        if(first == s1.size() and second == s2.size()) return true;
+        return false;
+    }
+    static bool cmp(string& s1, string& s2){
+        return s1.size() < s2.size();
     }
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(),words.end(),compare);
-        
+        sort(words.begin(), words.end(),cmp);
         int n=words.size();
-        unordered_map<string,int> m;
-        for(int i=0;i<n;i++) m[words[i]] = i;
         
-        vector<int> dp(n+1,1);
-        int ans = 1;
+        vector<int> dp(n, 1);
+        int maxi=1;
         for(int i=0;i<n;i++){
-            for(int j=0;j<words[i].size();j++){
-                string s = words[i].substr(0,j) + words[i].substr(j+1);
-                if(m.find(s) != m.end()){
-                    dp[m[s]] = max(dp[m[s]],1+dp[i]);
-                    ans = max(ans,dp[m[s]]);
+            for(int prev=0;prev<i;prev++){
+                if(check(words[i], words[prev]) and 1+dp[prev] > dp[i]){
+                    dp[i] = 1+dp[prev];
                 }
             }
+            maxi = max(maxi, dp[i]);
         }
-        return ans;
+        return maxi;
     }
 };
