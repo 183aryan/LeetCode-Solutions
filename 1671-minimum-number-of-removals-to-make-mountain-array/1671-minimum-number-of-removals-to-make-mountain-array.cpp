7858ea67b278bbcb4c20131dc95f183361d8c803
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-09-28 23:25:46 +0530

    Time: 754 ms (6.79%), Space: 12.1 MB (59.41%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -7,13 +7,13 @@ class Solution
             vector<int> dp1(n, 1);
             vector<int> dp2(n, 1);
 
-            for(int i=1;i<n;i++){
+            for(int i=0;i<n;i++){
                 for(int prev=0;prev<i;prev++){
                     if(nums[prev] < nums[i] and 1+dp1[prev] > dp1[i]) dp1[i] = 1+dp1[prev];
                 }
             }
             
-            for(int i=n-2;i>=0;i--){
+            for(int i=n-1;i>=0;i--){
                 for(int prev=n-1;prev>i;prev--){
                     if(nums[prev] < nums[i] and 1+dp2[prev] > dp2[i]) dp2[i] = 1+dp2[prev];
                 }
