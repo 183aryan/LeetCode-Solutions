46682953133a046fcd2304613d09f9a822be7f12
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-10-02 00:00:59 +0530

    Time: 2 ms (67.17%), Space: 6.1 MB (75.45%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int f(int ind, string& s, vector<int>& dp){
+        if(s[ind] == '0') return 0;
+        if(ind >= s.size()) return 1;
+        if(dp[ind] != -1) return dp[ind];
+        int one = f(ind+1, s, dp);
+        int two = 0;
+        if(ind+1 < s.size() and (s[ind] == '1' || (s[ind] == '2' and s[ind+1] <= '6'))){
+            two = f(ind+2, s, dp);
+        }
+        return dp[ind] = one + two;
+    }
+    int numDecodings(string s) {
+        if(s[0] == '0') return 0;
+        vector<int> dp(s.size(), -1);
+        return f(0, s, dp);
+    }
+};
