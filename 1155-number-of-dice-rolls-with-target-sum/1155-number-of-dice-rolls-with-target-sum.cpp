83ee472f97e3c2cf1838bed87e10fefa8e667a2e
ARYAN <76700307+183aryan@users.noreply.github.com>
2022-10-02 23:04:57 +0530

    Time: 70 ms (32.00%), Space: 8.7 MB (21.99%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -16,7 +16,19 @@ public:
         return dp[n][target] = take%mod;
     }
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n+1, vector<int> (target+1, -1));
-        return f(n, k, target, dp);
+        vector<vector<int>> dp(n+2, vector<int> (target+2, 0));
+        // return f(n, k, target, dp);
+        dp[0][0] = 1;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                long long take=0;
+                for(int ind=1;ind<=k;ind++){
+                    if(ind <= j) take += (dp[i-1][j-ind])%mod;
+                }
+                dp[i][j] = take%mod;
+            }
+        }
+        return dp[n][target];
     }
 };
