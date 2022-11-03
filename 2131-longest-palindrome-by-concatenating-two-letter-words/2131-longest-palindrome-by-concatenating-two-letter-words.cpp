880e2f4d93c745623c533a1fdbf89b8cfaeea8ff
ARYAN <aryan183choudhary@gmail.com>
2022-11-03 09:46:32 +0530

    Time: 344 ms (95.43%), Space: 167.9 MB (81.81%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,47 +1,29 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-//         unordered_map<string, int> um;
-//         for(int i=0;i<words.size();i++){
-//             um[words[i]]++;
-//         }
-        
-//         bool flag = false;
-//         for(auto it:um){
-//             string s = it.first;
-//             string t = s;
-//             reverse(t.begin(), t.end());
-//             if(s == t){
-//                 if(it.second%2) flag = true;
-//             }
-//             else{
-//                 if(um.find(t) != um.end()){
-//                     ans += min(it.second, um[t]);
-//                     um.erase(t);
-//                 }
-//             }
-//         
-        vector<vector<int>> check(26, vector<int> (26,0));
-        int cnt = 0;
-        
-        for(auto it:words){
-            int first = it[0] - 'a';
-            int second = it[1] - 'a';
-            // cout << first <<" "<< second;
-            
-            if(check[second][first]){
-                cnt += 4;
-                check[second][first]--;
-            }
-            else check[first][second]++;
+        unordered_map<string, int> um;
+        for(int i=0;i<words.size();i++){
+            um[words[i]]++;
         }
-        
-        for(int i=0;i<26;i++){
-            if(check[i][i]){
-                cnt += 2;
-                break;
+        int ans=0;
+        bool flag = false;
+        for(auto it:um){
+            string s = it.first;
+            string t = s;
+            reverse(t.begin(), t.end());
+            if(s == t){
+                 ans += it.second/2; 
+                 if(it.second%2) flag = true;
             }
-        }
-        return cnt;
+            else{
+                if(um.find(t) != um.end()){
+                    ans += min(it.second, um[t]);
+                    um.erase(t);
+                }
+            }
+         }
+         ans *= 4;
+         if(flag) ans += 2;
+         return ans;
     }
 };
