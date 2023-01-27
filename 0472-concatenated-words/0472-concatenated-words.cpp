0910a385c1feb358c00f983285c8680da9ba5ba4
ARYAN <aryan183choudhary@gmail.com>
2023-01-27 18:37:08 +0530

    Time: 520 ms (28.31%), Space: 119.3 MB (38.18%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        vector<string> v;
+        unordered_map<string, int> um;
+        
+        for(auto it:words){
+            um[it]++;
+        }
+        
+        for(auto it:words){
+            int n=it.size();
+            vector<int> dp(n+1, 0);
+            dp[0]=1;
+            for(int i=0;i<n;i++){
+                if(dp[i] == 0) continue;
+                for(int j=i;j<=n;j++){
+                    if(j-i<n and um[it.substr(i, j-i)] > 0) dp[j] = 1;
+                }
+                if(dp[n]){
+                    v.push_back(it);
+                    break;
+                }
+            }
+        }
+        return v;
+    }
+};
