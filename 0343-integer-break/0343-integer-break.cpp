6cb5a0edc75eb19f57d2e2e0dd4f21edcb1400c8
ARYAN <aryan183choudhary@gmail.com>
2023-02-07 10:49:08 +0530

    Time: 0 ms (100.00%), Space: 6 MB (66.81%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        if(n == 2) return 1;
-        if(n == 3) return 2;
-        
-        int prod = 1;
-        while(n > 4){
-            prod *= 3;
-            n -= 3;
+        int dp[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        if(n >= 3) dp[3] = 2;
+        if(n >= 4) dp[4] = 4;
+        for (int i = 5; i <= n; ++i) {
+            dp[i] = 3 * max(i - 3, dp[i - 3]);
         }
-        prod *= n;
-        
-        return prod;
+        return dp[n];
     }
 };
