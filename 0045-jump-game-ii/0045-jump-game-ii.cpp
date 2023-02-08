6124f01e43acc486160a6f81e74ffc5e4ce93e98
ARYAN <aryan183choudhary@gmail.com>
2023-02-08 11:41:27 +0530

    Time: 387 ms (30.92%), Space: 17.7 MB (15.91%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,36 @@
 #include<bits/stdc++.h>
 class Solution {
 public:
-    int f(int ind, vector<int> &nums, vector<int> &dp){
-        if(ind >= nums.size()-1) return 0;
+//     int f(int ind, vector<int> &nums, vector<int> &dp){
+//         if(ind >= nums.size()-1) return 0;
         
-        if(dp[ind]  != -1) return dp[ind];
+//         if(dp[ind]  != -1) return dp[ind];
         
-        long long int mini = INT_MAX;
+//         long long int mini = INT_MAX;
         
-        for(int i=1;i<=nums[ind];i++){
-            mini = min(mini, (long long)1+f(ind+i, nums, dp));
-        }
-        return dp[ind] = mini;
-    }
+//         for(int i=1;i<=nums[ind];i++){
+//             mini = min(mini, (long long)1+f(ind+i, nums, dp));
+//         }
+//         return dp[ind] = mini;
+//     }
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n+1, -1);
-        return f(0, nums, dp);
+        vector<long long int> dp(n+1, INT_MAX);
+        // return f(0, nums, dp);
+        dp[n-1] = 0;
+        for(int i=n-2;i>=0;i--){
+            long long int mini = INT_MAX;
+            bool flag=false;
+            for(int j=1;j<=nums[i];j++){
+                if(i+j >= n-1){
+                    dp[i] = 1;
+                    flag = true;
+                    break;
+                }
+                mini = min(mini, 1+dp[i+j]);
+                dp[i] = mini;
+            }
+        }
+        return dp[0];
     }
 };
