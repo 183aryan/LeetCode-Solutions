3cdd5c48db80048a6955bbd40b46b502d7751a5f
ARYAN <aryan183choudhary@gmail.com>
2023-04-13 08:09:43 +0530

    Time: 8 ms (68.46%), Space: 15.2 MB (98.01%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -6,13 +6,13 @@ public:
         
         for(auto it:pushed){
             pushed[i] = it;
-            i++; // so thaat if top does not matches then it should not modify any other value...
+            i++; // so that if top does not matches then it should not modify any other value...
             while(i > 0 and pushed[i-1] == popped[j]){
                 i--;
                 j++;
             }
         }
         
-        return i == 0;
+        return i == 0;   // time - O(N), space - O(1), modified space O(N) 
     }
 };
