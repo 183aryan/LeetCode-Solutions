932776b3b7fc33c50f91f2a6a5cce77f2656757f
ARYAN <aryan183choudhary@gmail.com>
2023-05-31 11:18:41 +0530

    Time: 169 ms (34.59%), Space: 59.3 MB (31.08%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -0,0 +1,30 @@
+class UndergroundSystem {
+public:
+    unordered_map<int, pair<string, int>> checkIns;
+    unordered_map<string, pair<int, int>> route;
+    UndergroundSystem() {
+        
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        checkIns[id] = {stationName, t};
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        string startEnd = checkIns[id].first + '-' + stationName;
+        route[startEnd] = {route[startEnd].first + (t - checkIns[id].second), route[startEnd].second + 1};
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        pair<int, int> p = route[startStation + '-' + endStation];
+        return (double)p.first / p.second;
+    }
+};
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
