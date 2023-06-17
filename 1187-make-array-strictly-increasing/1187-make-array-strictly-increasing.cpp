fc3115a259668ec61da69d4a98a515009b24ef22
ARYAN <aryan183choudhary@gmail.com>
2023-06-17 16:35:54 +0530

    Time: 1052 ms (9.88%), Space: 78.3 MB (11.63%) - LeetHub

---
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int f(int i, int prev, vector<int>& arr1, vector<int>& arr2, map<pair<int, int>, int>& dp){
+        if(i >= arr1.size()) return 0;
+        
+        if(dp.find({i, prev}) != dp.end()) return dp[{i, prev}];
+        
+        int ans = INT_MAX / 2;
+        
+        int id = upper_bound(arr2.begin(), arr2.end(), prev) - arr2.begin();
+        
+        if(arr1[i] > prev) ans = min(ans, f(i+1, arr1[i], arr1, arr2, dp));
+        if(id < arr2.size()) ans = min(ans, f(i+1, arr2[id], arr1, arr2, dp)+1);
+        
+        return dp[{i, prev}] = ans;
+    }
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+        sort(arr2.begin(), arr2.end());
+        map<pair<int, int>, int> dp;
+        int res = f(0, INT_MIN, arr1, arr2, dp);
+        
+        return res >= INT_MAX/2 ? -1 : res; 
+    }
+};
