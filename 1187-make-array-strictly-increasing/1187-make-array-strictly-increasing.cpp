2c1d76b31ec89c11503f9deb436ed0512fff0119
ARYAN <aryan183choudhary@gmail.com>
2023-06-17 16:41:00 +0530

    Time: 1080 ms (8.14%), Space: 78.1 MB (13.37%) - LeetHub

--- 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -5,12 +5,12 @@ public:
         
         if(dp.find({i, prev}) != dp.end()) return dp[{i, prev}];
         
-        int ans = INT_MAX / 2;
+        int ans = INT_MAX;
         
         int id = upper_bound(arr2.begin(), arr2.end(), prev) - arr2.begin();
         
         if(arr1[i] > prev) ans = min(ans, f(i+1, arr1[i], arr1, arr2, dp));
-        if(id < arr2.size()) ans = min(ans, f(i+1, arr2[id], arr1, arr2, dp)+1);
+        if(id < arr2.size()) ans = min(1LL*ans, 1LL*f(i+1, arr2[id], arr1, arr2, dp)+1);
         
         return dp[{i, prev}] = ans;
     }
@@ -19,6 +19,6 @@ public:
         map<pair<int, int>, int> dp;
         int res = f(0, INT_MIN, arr1, arr2, dp);
         
-        return res >= INT_MAX/2 ? -1 : res; 
+        return res >= INT_MAX ? -1 : res; 
     }
 };
