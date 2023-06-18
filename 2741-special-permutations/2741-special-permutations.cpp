0e324d9bb4bdb883141f43f4d0a54079f81f5a75
ARYAN <aryan183choudhary@gmail.com>
2023-06-18 21:36:52 +0530

    Time: 502 ms (87.50%), Space: 326.4 MB (12.50%) - LeetHub

---
+++ 2741-special-permutations/2741-special-permutations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int f(vector<int>& nums, int prevIndex, int currentIndex, int mask) {
+        if (currentIndex == nums.size()) {
+            return 1;
+        }
+        if (dp[prevIndex + 1][mask] != -1) {
+            return dp[prevIndex + 1][mask];
+        }
+        int tot = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            if (mask & (1 << j)) {
+                continue;
+            }
+            if (prevIndex == -1 || nums[prevIndex] % nums[j] == 0 || nums[j] % nums[prevIndex] == 0) {
+                tot += f(nums, j, currentIndex + 1, mask | (1 << j));
+                tot %= 1000000007;
+            }
+        }
+        dp[prevIndex + 1][mask] = tot;
+        return dp[prevIndex + 1][mask];
+    }
+
+    int specialPerm(vector<int>& nums) {
+        dp.resize(20,vector<int>((1 << 14) + 5, -1));
+        return f(nums, -1, 0, 0);
+    }
+};
