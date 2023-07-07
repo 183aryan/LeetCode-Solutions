485574769d48e0e7ae8a83b9f6a997693588e6dc
ARYAN <aryan183choudhary@gmail.com>
2023-07-08 00:31:00 +0530

    Time: 40 ms (77.54%), Space: 18.9 MB (93.41%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size()==0) return {};
+        vector<vector<int>> result;
+        sort(intervals.begin(),intervals.end());
+        result.push_back(intervals[0]);
+        int j=0;
+        for(int i=1;i<intervals.size();i++){
+            if(result[j][1]>=intervals[i][0]) result[j][1]=max(result[j][1],intervals[i][1]);
+            else{
+                j++;
+                result.push_back(intervals[i]);
+            }
+        }
+        return result;
+    }
+};
