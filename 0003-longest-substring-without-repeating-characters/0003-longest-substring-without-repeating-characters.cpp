066c2df307b7f97df510db658f72f542a10d49dc
ARYAN <aryan183choudhary@gmail.com>
2023-07-12 00:01:01 +0530

    Time: 30 ms (41.12%), Space: 9 MB (39.61%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-    //     unordered_map<char, int> um;
-    //     int maxi = 0;
-    //     int curr = 0;
-    //     int i=0, j=0;
-    //     while(j < s.size()){
-    //         if(um[s[j]] == 0){
-    //             curr++;
-    //             maxi = max(maxi, j-i+1);
-    //             um[s[j]]++;
-    //             j++;
-    //         }
-    //         else {
-    //             curr = 0;
-    //             i=j;
-    //         }
-    //     }
-    //     return maxi;
-    // }
-        vector<int> v(256, 0);
-        int i=0, j=0;
+        unordered_map<char, int> um;
         int maxi = 0;
+        int curr = 0;
+        int i=0, j=0;
         while(j < s.size()){
-            v[s[j]]++;
-            while(v[s[j]] > 1){
-                v[s[i++]]--;
+            um[s[j]]++;
+            
+            if(um[s[j]] > 1){
+                while(um[s[j]] > 1){
+                    um[s[i]]--;
+                    if(um[s[i]] == 0) um.erase(s[i]);
+                    i++;
+                }
             }
-            maxi = max(maxi, j-i+1);
+            else maxi = max(maxi, j-i+1);
             j++;
         }
         return maxi;
     }
+    //     vector<int> v(256, 0);
+    //     int i=0, j=0;
+    //     int maxi = 0;
+    //     while(j < s.size()){
+    //         v[s[j]]++;
+    //         while(v[s[j]] > 1){
+    //             v[s[i++]]--;
+    //         }
+    //         maxi = max(maxi, j-i+1);
+    //         j++;
+    //     }
+    //     return maxi;
+    // }
 };
