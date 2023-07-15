03219317d901e561790b65771cce3bf51e9d453e
ARYAN <aryan183choudhary@gmail.com>
2023-07-15 22:40:37 +0530

    Time: 11 ms (7.89%), Space: 11 MB (92.42%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo=0,hi=nums.size()-1,mid;
+        while(hi>=lo){
+            mid = lo + (hi-lo)/2;
+            if(nums[mid] == target) 
+                return mid;
+            if(nums[lo] <= nums[mid]){
+                if(target>=nums[lo] and target < nums[mid]){
+                    hi = mid-1;
+                }
+                else lo = mid + 1;
+            }
+            else{      
+                if(target > nums[mid] and target <= nums[hi]){
+                    lo = mid + 1;
+                }
+                else hi = mid-1;
+            }
+        }
+        return -1;
+    }
+};
