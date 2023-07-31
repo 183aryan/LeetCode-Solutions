2c8350a0abf9b5a07f8fa90b568471120ebac337
ARYAN <aryan183choudhary@gmail.com>
2023-07-31 17:01:26 +0530

    Time: 7 ms (74.75%), Space: 11.7 MB (58.38%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -13,20 +13,32 @@ public:
     ListNode* rotateRight(ListNode* head, int k) {
         if(head == NULL || head->next == NULL) return head;
         ListNode* t = head;
-        int len=0;
-        while(t != NULL){
-            len++;
+        // int len=0;
+        // while(t != NULL){
+        //     len++;
+        //     t = t->next;
+        // }
+        // k = k%len;
+        // for(int i=0;i<k;i++){
+        //     ListNode* curr = head;
+        //     while(curr->next->next != NULL) curr = curr->next;
+        //     ListNode* x = curr->next;
+        //     curr->next = NULL;
+        //     x->next = head;
+        //     head = x;
+        // }
+        // return head;
+        int len=1;
+        while(t->next){
             t = t->next;
+            len++;
         }
-        k = k%len;
-        for(int i=0;i<k;i++){
-            ListNode* curr = head;
-            while(curr->next->next != NULL) curr = curr->next;
-            ListNode* x = curr->next;
-            curr->next = NULL;
-            x->next = head;
-            head = x;
-        }
+        t->next = head;
+        k %= len;
+        k = len - k;
+        while(k--) t = t->next;
+        head = t->next;
+        t->next = NULL;
         return head;
     }
 };
