a2f97fc453b069a7c83221963859f516c4745ccf
ARYAN <aryan183choudhary@gmail.com>
2023-08-02 21:25:58 +0530

    Time: 102 ms (20.94%), Space: 5.9 MB (96.47%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s = "";
+        for(int i=1;i<=n;i++) s += (i + '0');
+        
+        while(k != 1){
+            next_permutation(s.begin(), s.end());
+            k--;
+        }
+        return s;
+   }
+};
