a0849e593f83640eb0e53cb125dd9a531fa6c68e
ARYAN <aryan183choudhary@gmail.com>
2023-08-02 11:07:21 +0530

    Time: 4 ms (48.08%), Space: 7.5 MB (97.00%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
+    void f(int index, vector<int>& nums, vector<vector<int>>& v){
+        if(index == nums.size()){
+            v.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[i], nums[index]);
+            f(index+1, nums, v);
+            swap(nums[i], nums[index]);
+        }
+    }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> v;
-        sort(nums.begin(), nums.end());
-        v.push_back(nums);
+        // USING STL
+//         sort(nums.begin(), nums.end());
+//         v.push_back(nums);
         
-        while(next_permutation(nums.begin(), nums.end())){
-            v.push_back(nums);
-        }
+//         while(next_permutation(nums.begin(), nums.end())){
+//             v.push_back(nums);
+//         }
+//         return v;
+        
+        // Backtracking
+        f(0, nums, v);
         return v;
     }
 };
